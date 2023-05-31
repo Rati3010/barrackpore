e9c4935e8ce6605e29f05369d44ce91fb199a885
Rati3010 <mratikanta1997@gmail.com>
2023-05-31 22:37:36 +0530

    added some functionality

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+// import '@fullcalendar/core/main.css';
+// import '@fullcalendar/daygrid/main.css';
+// import '@fullcalendar/timegrid/main.css';
+import RenderEvent from './components/RenderEvent';
+
+const events = [
+  { title: 'Meeting', start: new Date() }
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demo App</h1>
+      <FullCalendar
+        plugins={[timeGridPlugin]}
+        initialView='timeGridWeek'
+        weekends={false}
+        events={events}
+        eventContent={RenderEvent}
+      />
     </div>
   );
 }
---
+++ calender/src/components/RenderEvent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RenderEvent = () => {
+  return (
+    <>
+      <b>timeText</b>
+      <b>title</b>
+    </>
+  );
+};
+
+export default RenderEvent;
